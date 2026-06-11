c5b84101138c414aa0852c8fd7b631e0c6d27f01
CJ0968-JIA <jiac0968@gmail.com>
2026-06-12 05:40:36 +0800

    完成專案

--- order-system/vite.config.js
+++ order-system/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   base: '/0612/',
+  base: '/0612/',   // ← 加這行，和倉庫名稱一樣
 })
